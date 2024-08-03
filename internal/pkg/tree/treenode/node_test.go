6c00cf4e6afa58c3792a668ae9d1b946d5e17d7d
cozmo <yu.zh@hotmail.com>
2024-08-04 01:49:00 +0800

    fix node_test

--- internal/pkg/tree/treenode/node_test.go
+++ internal/pkg/tree/treenode/node_test.go
@@ -2,6 +2,7 @@ package treenode
 
 import (
 	"github.com/cozmo-zh/zearches/consts"
+	"github.com/cozmo-zh/zearches/internal/pkg/tree/mocks"
 	"github.com/cozmo-zh/zearches/pkg/bounds"
 	"github.com/cozmo-zh/zearches/pkg/siface"
 	"testing"
@@ -9,26 +10,6 @@ import (
 	"github.com/cozmo-zh/zearches/pkg/geo"
 	"github.com/stretchr/testify/assert"
 )
-
-type MockSpatial struct {
-	id       int64
-	location geo.Vec3Int
-}
-
-func (m *MockSpatial) GetID() int64 {
-	return m.id
-}
-
-func (m *MockSpatial) GetLocation() geo.Vec3Int {
-	return m.location
-}
-
-func createMockSpatial(id int64, x, y, z int32) siface.ISpatial {
-	return &MockSpatial{
-		id:       id,
-		location: geo.NewVec3Int(x, y, z),
-	}
-}
 
 func TestNewTreeNode(t *testing.T) {
 	maxDepth := 4
@@ -47,8 +28,8 @@ func TestAddEntity(t *testing.T) {
 	b := bounds.NewBound(geo.NewVec3Int(0, 0, 0), geo.NewVec3Int(10, 10, 10))
 	node, _ := NewTreeNode(consts.Dim3, nil, b, 0, maxDepth, capacity)
 
-	spatial1 := createMockSpatial(1, 2, 2, 2)
-	spatial2 := createMockSpatial(2, 8, 8, 8)
+	spatial1 := mocks.CreateMockSpatial(1, 2, 2, 2)
+	spatial2 := mocks.CreateMockSpatial(2, 8, 8, 8)
 
 	node.Add(spatial1)
 	node.Add(spatial2)
@@ -65,7 +46,7 @@ func TestRemoveEntity(t *testing.T) {
 	b := bounds.NewBound(geo.NewVec3Int(0, 0, 0), geo.NewVec3Int(10, 10, 10))
 	node, _ := NewTreeNode(consts.Dim3, nil, b, 0, maxDepth, capacity)
 
-	spatial := createMockSpatial(1, 5, 5, 5)
+	spatial := mocks.CreateMockSpatial(1, 5, 5, 5)
 	node.Add(spatial)
 	removed := node.Remove(spatial.GetID())
 
@@ -79,8 +60,8 @@ func TestDivideIf(t *testing.T) {
 	b := bounds.NewBound(geo.NewVec3Int(0, 0, 0), geo.NewVec3Int(10, 10, 10))
 	node, _ := NewTreeNode(consts.Dim3, nil, b, 0, maxDepth, capacity)
 
-	spatial1 := createMockSpatial(1, 2, 2, 2)
-	spatial2 := createMockSpatial(2, 8, 8, 8)
+	spatial1 := mocks.CreateMockSpatial(1, 2, 2, 2)
+	spatial2 := mocks.CreateMockSpatial(2, 8, 8, 8)
 	node.Add(spatial1)
 	node.Add(spatial2)
 	assert.False(t, node.IsLeaf())
@@ -93,7 +74,7 @@ func TestContains(t *testing.T) {
 	b := bounds.NewBound(geo.NewVec3Int(0, 0, 0), geo.NewVec3Int(10, 10, 10))
 	node, _ := NewTreeNode(consts.Dim3, nil, b, 0, maxDepth, capacity)
 
-	spatial := createMockSpatial(1, 5, 5, 5)
+	spatial := mocks.CreateMockSpatial(1, 5, 5, 5)
 	contains := node.Contains(spatial)
 
 	assert.True(t, contains)
@@ -115,14 +96,14 @@ func TestMergeIf(t *testing.T) {
 	parent, _ := NewTreeNode(consts.Dim3, nil, b, 0, maxDepth, capacity)
 	assert.True(t, parent.IsLeaf())
 	spatials := []siface.ISpatial{
-		createMockSpatial(1, 1, 1, 1),
-		createMockSpatial(2, 2, 2, 2),
-		createMockSpatial(3, 3, 3, 3),
-		createMockSpatial(4, 4, 4, 4),
-		createMockSpatial(5, 5, 5, 5),
-		createMockSpatial(6, 6, 6, 6),
-		createMockSpatial(7, 7, 7, 7),
-		createMockSpatial(8, 8, 8, 8),
+		mocks.CreateMockSpatial(1, 1, 1, 1),
+		mocks.CreateMockSpatial(2, 2, 2, 2),
+		mocks.CreateMockSpatial(3, 3, 3, 3),
+		mocks.CreateMockSpatial(4, 4, 4, 4),
+		mocks.CreateMockSpatial(5, 5, 5, 5),
+		mocks.CreateMockSpatial(6, 6, 6, 6),
+		mocks.CreateMockSpatial(7, 7, 7, 7),
+		mocks.CreateMockSpatial(8, 8, 8, 8),
 	}
 	for _, spatial := range spatials {
 		parent.Add(spatial)
@@ -142,15 +123,15 @@ func TestAddEntityAtMaxDepth(t *testing.T) {
 
 	// Create spatial entities to fill the tree to its maximum depth
 	spatials := []siface.ISpatial{
-		createMockSpatial(1, 1, 1, 1),
-		createMockSpatial(2, 2, 2, 2),
-		createMockSpatial(3, 3, 3, 3),
-		createMockSpatial(4, 4, 4, 4),
-		createMockSpatial(5, 5, 5, 5),
-		createMockSpatial(6, 6, 6, 6),
-		createMockSpatial(7, 7, 7, 7),
-		createMockSpatial(8, 8, 8, 8),
-		createMockSpatial(9, 9, 9, 9),
+		mocks.CreateMockSpatial(1, 1, 1, 1),
+		mocks.CreateMockSpatial(2, 2, 2, 2),
+		mocks.CreateMockSpatial(3, 3, 3, 3),
+		mocks.CreateMockSpatial(4, 4, 4, 4),
+		mocks.CreateMockSpatial(5, 5, 5, 5),
+		mocks.CreateMockSpatial(6, 6, 6, 6),
+		mocks.CreateMockSpatial(7, 7, 7, 7),
+		mocks.CreateMockSpatial(8, 8, 8, 8),
+		mocks.CreateMockSpatial(9, 9, 9, 9),
 	}
 
 	// Add entities to the tree
@@ -165,9 +146,9 @@ func TestFindEntitiesReturnsEntitiesWithinRadius(t *testing.T) {
 	b := bounds.NewBound(geo.NewVec3Int(0, 0, 0), geo.NewVec3Int(10, 10, 10))
 	node, _ := NewTreeNode(consts.Dim3, nil, b, 0, maxDepth, capacity)
 
-	spatial1 := createMockSpatial(1, 4, 4, 4)
-	spatial2 := createMockSpatial(2, 8, 8, 8)
-	spatial3 := createMockSpatial(3, 5, 5, 5)
+	spatial1 := mocks.CreateMockSpatial(1, 4, 4, 4)
+	spatial2 := mocks.CreateMockSpatial(2, 8, 8, 8)
+	spatial3 := mocks.CreateMockSpatial(3, 5, 5, 5)
 	node.Add(spatial1)
 	node.Add(spatial2)
 	node.Add(spatial3)
