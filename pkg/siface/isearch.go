e51f2027dec6d947afc9b7d470c8390ca1866319
cozmo <yu.zh@hotmail.com>
2024-08-04 16:41:29 +0800

    add comments

--- pkg/siface/isearch.go
+++ pkg/siface/isearch.go
@@ -1,8 +1,12 @@
 // Package siface .
 package siface
 
+// ISearch  interface for search, like Octree, QuadTree, RTree, etc.
 type ISearch interface {
+	// Add adds an entity to the search tree.
 	Add(entity ISpatial) bool
+	// Remove removes an entity from the search tree by its ID.
 	Remove(entityId int64) bool
+	// GetSurroundingEntities finds entities within a certain radius of a center point.
 	GetSurroundingEntities(center []float32, radius float32, filters ...func(entity ISpatial) bool) []ISpatial
 }
